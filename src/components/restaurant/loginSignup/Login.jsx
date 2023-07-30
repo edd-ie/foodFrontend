71b7ca325b939652245495731d7b2378ec7aab05
Mohamed <mohasalanka2020@gmail.com>
2023-07-30 22:41:18 +0300

    Done modification on login

--- src/components/restaurant/loginSignup/Login.jsx
+++ src/components/restaurant/loginSignup/Login.jsx
@@ -2,89 +2,80 @@ import React, { useState } from 'react';
 import './Login.css';
 import logo from '../../../assets/logo.svg';
 
-
-
-
-export default function CustomerSignUp({ user, setUser, setLogin, login }) {
-
+export default function RestaurantLogin({ user, setUser, setLogin, login }) {
   const [formData, setFormData] = useState({
-    name: '',
     username: '',
-    email: '',
     password: '',
-    agreeTerms: false,
   });
 
   const handleChange = (event) => {
-    const { name, value, type, checked } = event.target;
-    const newValue = type === 'checkbox' ? checked : value;
-    setFormData((prevFormData) => ({ ...prevFormData, [name]: newValue }));
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
   };
-
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    // Perform restaurant login logic using the formData
     console.log(formData);
   };
 
   const handleForgotPassword = () => {
-
     console.log("Forgot Password clicked!");
   };
 
   return (
     <div id="moLand">
-    <div id='moLogin'>
-    <img id='moLogo'  src={logo} alt="logo" />
-      <h1>Login to your account</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label id='moUsername' htmlFor='username'>Username:</label>
-          <input 
-            type='text'
-            id='mohausername'
-            name='username'
-            value={formData.username}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label id='moPassword' htmlFor='password'>Password:</label>
-          <input
-            type='password'
-            id='mohapassword'
-            name='password'
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
+      <div id='moLogin'>
+        <img id='moLogo' src={logo} alt="logo" />
+        <h1>Restaurant Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label id='moUsername' htmlFor='username'>Username:</label>
+            <input 
+              type='text'
+              id='mohausername'
+              name='username'
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label id='moPassword' htmlFor='password'>Password:</label>
+            <input
+              type='password'
+              id='mohapassword'
+              name='password'
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-        <div id='moForgot'>
-        <p>
+          <div id='moForgot'>
+            <p>
+              <span
+                style={{ cursor: 'pointer', color: 'orange' }}
+                onClick={handleForgotPassword}
+              >
+                Forgot Password?
+              </span>
+            </p>
+          </div>
+          <button id='moButton' type='submit'>Login</button>
+        </form>
+        <div id="mohaLogin">
+          <p>
+            Don't have an account?  
             <span
-              style={{ cursor: 'pointer', color: 'orange' }}
-              onClick={handleForgotPassword}
+              style={{ cursor: 'pointer', color: 'orange'}}
+              onClick={() => setLogin(!login)}
             >
-              Forgot Password?
+              Create account
             </span>
           </p>
         </div>
-        <button id='moButton' type='submit'>Login</button>
-      </form>
-      <div id="mohaLogin">
-      <p>
-          Don't have an account?  
-          <span
-            style={{ cursor: 'pointer', color: 'orange'}}
-            onClick={() => setLogin(!login)}
-          >
-          Create account
-          </span>
-        </p>
       </div>
     </div>
-    </div>
-  );
+  );
 }
