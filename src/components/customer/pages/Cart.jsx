8002f1fae52e9d9c9ea191110c693962a2829550
Mohamed <mohasalanka2020@gmail.com>
2023-08-01 01:05:08 +0300

    first commit on cart

--- src/components/customer/pages/Cart.jsx
+++ src/components/customer/pages/Cart.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import './cart.css'
+import './cart.css';
 
-
-export default function Cart({user, setUser, setLogin, login}) {
-
-    return(
+export default function Cart({ user, setUser, setLogin, login }) {
+  return (
+    <div>
+      {/* Your cart content goes here */}
+      {/* For example, you can use the previous menu items */}
+      <div>
         <div>
-            Mohamed
+          <span>Mango Masai</span>
+          <span>Ksh 1260</span>
         </div>
-    )
+        <div>
+          <span>Chips</span>
+          <span>Ksh 700</span>
+        </div>
+        <div>Subtotal: Ksh 1960</div>
+        <div>Service Fee: Ksh 150</div>
+        <div>Total: Ksh 2110</div>
+        <button onClick={() => console.log('Proceed to Checkout')}>
+          Proceed to Checkout
+        </button>
+      </div>
+    </div>
+  );
 }
