8d6a0f44ad7457c165b930b13dbbb201bae23366
edd-ie <ogendoeddie@gmail.com>
2023-08-01 14:31:49 +0300

    Restaurant Nav

--- src/components/utility/NavR.jsx
+++ src/components/utility/NavR.jsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 import './navC.css'
 
 export default function NavR({user, setLogin}) {
-    
+    const [count, setCount] = React.useState(1)
+
 
     return(
         <div id="rootNav">
@@ -11,9 +12,17 @@ export default function NavR({user, setLogin}) {
             </div>
             <div className="rLink">Inventory</div>
             <div className="rLink">Staff</div>
-            <div className="rLink">Orders</div>
             <div className="rLink">Menu</div>
             <div className="rLink">History</div>
+            <div className="rLink rCart">
+                Orders 
+                <span class="material-symbols-sharp">
+                    restaurant_menu
+                </span>
+                <div className="cartCount">
+                    {count}
+                </div>
+            </div>
             <div className="rLink rProf"></div>
         </div>
     )
