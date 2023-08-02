d94b2d8b84e2d9944e231cae3c5859bdf8d2f6fc
edd-ie <ogendoeddie@gmail.com>
2023-08-02 13:27:35 +0300

    OrderTracking

--- src/components/utility/ProfC.jsx
+++ src/components/utility/ProfC.jsx
@@ -14,7 +14,27 @@ export default function ProfC({user}) {
             setProfile(data)
         })
     },[])
-    
+
+    function handleSubmit(e){
+        e.preventDefault()
+        let form = e.target
+        let data = {}
+        
+        for (let x of form){
+            if (x.name !== ""){
+                data[x.name] = x.value
+            }
+        }
+        
+        console.log("fileData: ", data);
+        
+
+        // fetch('https://backendfood-co7z.onrender.com/customers/1',{
+        //     method: 'PATCH',
+        //     headers: {'Content-Type': 'application/json'},
+        //     body: JSON.stringify({})
+        // })
+    }
 
     return(
         <div className="eProfileMain">
@@ -24,7 +44,15 @@ export default function ProfC({user}) {
                     <div className="eProfPic" >
                         <img src={profile.picture} alt="prof" id="eProfImg" />
                     </div>
-
+                    <div className="eProfForm">
+                        <form action="submit" className="eProfFrm" onSubmit={handleSubmit} >
+                            <input className="eProfFrm1" type="text" name="username" placeholder="username" />
+                            <input className="eProfFrm1" type="email" name="email" placeholder="email" />
+                            <input id="eText1" className="eProfFrm1" type="text" name="phone" placeholder="phone" />
+                            <input className="eProfFrm1" type="file" name="picture" placeholder="picture" />
+                            <button>Submit</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
