a7a5e15be748c37865e83b181c73802fc28205c0
edd-ie <ogendoeddie@gmail.com>
2023-08-07 19:24:58 +0300

    Contact form

---
+++ src/components/utility/contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './contact.css'
+
+export default function Contact({user}) {
+    
+
+    return(
+        <div className="contactPage">
+            
+        </div>
+    )
+}
