6823052e4ed7ce97a3a18792173f445dccff27b7
Nkathaglow <kinyua6945@gmail.com>
2023-08-09 18:18:40 +0300

    Working on the favourites table

--- src/components/customer/pages/Cart.jsx
+++ src/components/customer/pages/Cart.jsx
@@ -60,10 +60,7 @@ export default function Cart({ user, setUser, setLogin, login }) {
     setFood(prevFood => prevFood.filter(item => !deletedFoodIds.includes(item.id)));
   }, []);
 
-  const toggleShowTime = (index) => {
-    setShowTime(!showTime);
-    setSelectedFoodIndex(index);
-  };
+
 
   const handleQuantityChange = (foodId, action) => {
     setQuantities(prevQuantities => {
--- src/components/customer/pages/Favourites.jsx
+++ src/components/customer/pages/Favourites.jsx
@@ -1,14 +1,74 @@
 import React, { useEffect, useState } from 'react';
-import './Favourites.css'
+import './Favourites.css';
 import NavC from '../../utility/NavC';	
 
-export default function Favourites({user}) {
-    
+export default function Favourites({ user }) {
+    const [favRes, setFavRes] = useState([]);
 
+    useEffect(() => {
+        async function fetchFavRes() {
+            try {
+                const response = await fetch("https://backendfood-co7z.onrender.com/customer/favRes/1");
+                const data = await response.json();
+                setFavRes(data);
+            } catch (error) {
+                console.error("Error fetching data", error);
+            }
+        }
 
-    return(
+        fetchFavRes();
+    }, []);
+
+    function handleDelete(id) {
+        // Delete the restaurant from the database
+        fetch(`https://backendfood-co7z.onrender.com/customer/favRes/1${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        const newFavRes = favRes.filter((item) => item.id !== id);
+        setFavRes(newFavRes);
+    }
+
+    return (
         <div className="favourites">
             <NavC/>
+            <table id='favRes'>
+                <thead id="favHeader">
+                    <tr>
+                        <th>Picture</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Ambience</th>
+                        <th>Cuisine</th>
+                        <th>Ratings</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {favRes.map((item) => (
+                        <tr key={item.id}>
+                            <td>
+                                <img src={item.restaurant.picture} alt={`Picture of ${item.restaurant.name}`} />
+                            </td>
+                            <td>{item.restaurant.name}</td>
+                            <td>{item.restaurant.email}</td>
+                            <td>{item.restaurant.phone}</td>
+                            <td>{item.restaurant.ambience}</td>
+                            <td>{item.restaurant.cuisine}</td>
+                            <td>{item.restaurant.ratings}</td>
+                            <td>
+                                <button onClick={() => handleDelete(item.id)}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
+    );
+
+  
 }
