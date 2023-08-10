194db3482b1c8559400e4e267803f111d4be19f8
edd-ie <ogendoeddie@gmail.com>
2023-08-10 11:01:07 +0300

    verification

---
+++ src/components/utility/checkEmail.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import './checkEmail.css'
+
+export default function checkEmail({user}) {
+
+    return(
+        <div className="checkEmail">
+            <h1>Check your email</h1>
+            
+        </div>
+    )
+}
