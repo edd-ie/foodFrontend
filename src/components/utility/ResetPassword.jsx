4d9e4df6ab21cd6256b9858029d5882ec2466c10
Mohamed <mohasalanka2020@gmail.com>
2023-08-11 12:33:26 +0300

    finetuned reset password

--- src/components/utility/ResetPassword.jsx
+++ src/components/utility/ResetPassword.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import './ResetPassword.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function ResetPassword() {
     const [newPassword, setNewPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const navigate = useNavigate();
 
     const handleResetPassword = () => {
         // Simulate successful password reset (replace with actual reset logic)
         if (newPassword === confirmPassword) {
             console.log('Password reset successful');
+            navigate('/customer/login');
         } else {
             console.error('Passwords do not match');
         }
